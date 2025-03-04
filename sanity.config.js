1a644f7b3e9cfa241e1cb6ba463bc40582f192b8
Amit <amitkumar@rwit.io>
2025-03-04 17:38:05 +0530

    Added configurations in sanity config cli

--- sanity.config.js
+++ sanity.config.js
@@ -2,12 +2,14 @@ import {defineConfig} from 'sanity'
 import {structureTool} from 'sanity/structure'
 import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemaTypes'
+const dotenv = require('dotenv');
+dotenv.config();
 
 export default defineConfig({
   name: 'default',
   title: 'Sanity WordPress',
 
-  projectId: 'lgu75gpc',
+  projectId: process.env.PROJECT_ID,
   dataset: 'production',
 
   plugins: [structureTool(), visionTool()],
